89e7b8091407623bd1ce5a699fab64a0492c395d
tarakdata <138832342+tarakdata@users.noreply.github.com>
2023-07-06 20:32:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import  streamlit
 streamlit.title ('My parents new healthy diner') 
 
-Breakfast Menu
-Omega 3 and Blueberry oatmean
+BreakfastMenu
+Omega 3 & Blueberry oatmean
 kale spinach & Rocket smoothy
 Hard boiled free range egg
