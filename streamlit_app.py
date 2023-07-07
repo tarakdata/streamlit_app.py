b98a856c5eea5584a177ca213b696557907b6301
tarakdata <138832342+tarakdata@users.noreply.github.com>
2023-07-07 15:39:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,4 @@ streamlit.dataframe(my_fruit_list)
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
+streamlit.dataframe(fruits_to_show)
