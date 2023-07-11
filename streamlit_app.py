c7e37b67b140a0a9e9679fef1270e314f1c4d011
tarakdata <138832342+tarakdata@users.noreply.github.com>
2023-07-10 22:42:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,4 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response)
 
 streamlit.header("Fruityvice Fruit Advice!")
-sreamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json())
