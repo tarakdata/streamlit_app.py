aa0d670ebb4354a81a55096c2e65dca7d24bec48
tarakdata <138832342+tarakdata@users.noreply.github.com>
2023-07-20 23:20:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,4 +54,4 @@ def get_fruit_load_list():
 if streamlit.button('Get fruit load list'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows= get_fruit_load_list()  
-    streamlit.dataframe(my_data_rows)
+   streamlit.dataframe(my_data_rows)
