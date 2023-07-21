96d5fcc89f7b6dd78b9c5e4a8f12c867ceffad4a
tarakdata <138832342+tarakdata@users.noreply.github.com>
2023-07-20 22:57:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ streamlit.dataframe(fruits_to_show)
 
 #streamlit.text(fruityvice_response)
 
-        def get_fruityvice_data(this_fruit_choice)
+        def get_fruityvice_data(this_fruit_choice):
            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + this_fruit_choice)
            fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
         return fruityvice_normalized
